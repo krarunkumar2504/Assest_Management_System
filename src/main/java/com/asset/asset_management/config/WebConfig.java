6015d3468768013e6a2cd3e764c208e6e7d6f320
Arun K R <arunkumarkr252004>
2026-03-24 00:13:54 +0530

    fixed cors issuse

--- src/main/java/com/asset/asset_management/config/WebConfig.java
+++ src/main/java/com/asset/asset_management/config/WebConfig.java
@@ -1,10 +1,11 @@
 package com.asset.asset_management.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig {
@@ -15,7 +16,6 @@ public class WebConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
                         .allowedOrigins("https://asset-frontend-xi.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
